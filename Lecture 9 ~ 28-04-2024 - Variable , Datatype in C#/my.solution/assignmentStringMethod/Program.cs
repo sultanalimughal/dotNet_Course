c3ef268e3926e0cd5f842664abbf9d69fc8b3e5e
Sultan Ali Tipu <135333688+sultanali-mughal@users.noreply.github.com>
2024-05-03 15:10:30 +0500

    add manual layout and with bootstrap

---
+++ Lecture 9 ~ 28-04-2024 - Variable , Datatype in C#/my.solution/assignmentStringMethod/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// string length
+Console.WriteLine("Enter String ");
+string str = Console.ReadLine();
+int strlg = str.Length;
+Console.WriteLine("length is :" + strlg);
+
+
+//Concatenation
+Console.WriteLine("First String");
+string c = Console.ReadLine();
+Console.WriteLine("Second String");
+string d = Console.ReadLine();
+Console.WriteLine(c + d);
+
+
+//interpolation
+Console.WriteLine(string.Concat(c, d));
+string f = $"{c} {d}";
+Console.WriteLine(f);
+Console.WriteLine(c[0]);//Access String
+Console.WriteLine(c.IndexOf("e"));//indexof
+Console.WriteLine(f.LastIndexOf("g")); //last index of string
+Console.WriteLine(string.Compare(c, d)); //string compare
+Console.WriteLine(string.Equals(c, d));//string Equal
+Console.WriteLine(f.Replace(c, d));//string Replace
+Console.WriteLine(f.Contains("shahzad")); //string contains
+Console.WriteLine("enter text");
+string e = Console.ReadLine();
+Console.WriteLine(e.ToUpper());//To Upper
+Console.WriteLine(e.ToLower());// To Lower
+
---
+++ Lecture 9 ~ 28-04-2024 - Variable , Datatype in C#/my.solution/my.project/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+	//Console.WriteLine("Hello, World!");
+
+
+// Variable in C#
+
+// 1) Decleration 2) initialization 
+
+
+//string myFriendName;
+
+//myFriendName = "Mark";
+
+//myFriendName = null;
+
+//Console.WriteLine(myFriendName);
+
+
+	//Console.WriteLine("enter your friend name");
+
+	//string myFriendName = Console.ReadLine();
+
+
+	//Console.WriteLine("enter friend location");
+
+	//string country = Console.ReadLine(); 
+
+//myFriendName = "Mark";
+
+
+//String concatination 
+
+//Console.WriteLine("My Frind name is " + myFriendName );
+//Console.WriteLine("He is from USA");
+//Console.WriteLine("He is software engineering"); 
+//Console.WriteLine("He is 34 years old");
+
+
+
+//String interpolation 
+	//Console.WriteLine( $@"My Frind name is {myFriendName}");
+	//Console.WriteLine($@"He is from {country}");
+	//Console.WriteLine("He is software engineering");
+	//Console.WriteLine("He is 34 years old");
