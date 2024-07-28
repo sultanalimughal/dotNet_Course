579f766385d50586e8d43636bdf2a027750a5157
Sultan Ali Tipu <135333688+sultanali-mughal@users.noreply.github.com>
2024-07-28 18:54:41 +0500

    Lecture no 18

---
+++ Lecture 18 ~ 29-06-2024 - Polymorphism in C#/my.solution/DomainModels/AdminWorkContact.cs
@@ -0,0 +1,16 @@
+﻿using DomainModels.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModels
+{
+    public class AdminWorkContact : WorkContact
+    {
+        public AdminWorkContact(string _name, string _email, string designation) : base(_name, _email, designation , WorkContactTypes.Admin)
+        {
+        }
+    }
+}
---
+++ Lecture 18 ~ 29-06-2024 - Polymorphism in C#/my.solution/My.Infrastructure/StudentRepository.cs
@@ -0,0 +1,20 @@
+﻿using DomainModels.AbstractionUserCase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My.Infrastructure
+{
+    public class StudentRepository
+    {
+        public void StudentUAT()
+        {
+            var Student = new Student("Alan");
+            Console.WriteLine(Student);
+
+            Student.AddNumbers(2, 2 , 4);
+        }
+    }
+}
